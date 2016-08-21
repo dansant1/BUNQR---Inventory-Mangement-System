8ab7b9671ed88240afb54833394911d5fc5edcee
Daniel Delgadillo <danieldelgadilloh@gmail.com>
2016-08-21 02:11:22 -0500

    Se agrego chart para utilidad vs costos y chart estatico para productos mas vendidos

--- client/templates/backoffice/reportes.js
+++ client/templates/backoffice/reportes.js
@@ -257,11 +257,180 @@ Template.reporteDiario.onRendered(function () {
   		var reporteChart = new Chart(ctx).Bar(data, options);
 	}
 
+	// Reporte Utilidades vs Costos
+	function reporteUC () {
+
+		var obtenerUtilidades = function  () {
+
+		var utilidades = [];
+
+		// Obtenemos el total de cada mes
+		for (var mes = 0; mes <= 11; mes++) {
+
+			
+			var date = new Date();
+			var firstDay = new Date(date.getFullYear(), mes, 1);
+			var lastDay = new Date(date.getFullYear(), mes + 1, 0);
+
+			var utilidadTotalMes = 0;
+
+			Reportes.find({ fecha: { $gte: firstDay, $lt: lastDay } }).forEach( function (index) {
+
+				if (index.valorutilidadVenta === undefined) {
+					utilidadTotalMes += 0;
+				} else {
+					utilidadTotalMes += index.valorutilidadVenta;
+				}
+				
+			});
+			
+			utilidades.push(utilidadTotalMes);
+		}
+
+		return utilidades;
+	}
+
+	var obtenerCostos = function () {
+
+		var costos = [];
+
+		// Obtenemos el total de cada mes
+		for (var mes = 0; mes <= 11; mes++) {
+
+			
+			var date = new Date();
+			var firstDay = new Date(date.getFullYear(), mes, 1);
+			var lastDay = new Date(date.getFullYear(), mes + 1, 0);
+
+			var costoTotalMes = 0;
+
+			Reportes.find({ fecha: { $gte: firstDay, $lt: lastDay } }).forEach( function (index) {
+
+				if (index.valorCosto === undefined) {
+					costoTotalMes += 0;
+				} else {
+					costoTotalMes += index.valorCosto;
+				}
+				
+			});
+			
+			costos.push(costoTotalMes);
+		}
+
+		return costos;
+	}
+
+		var options = {
+
+			///Boolean - Whether grid lines are shown across the chart
+			scaleShowGridLines: true,
+
+			//String - Colour of the grid lines
+			scaleGridLineColor: "rgba(0,0,0,.05)",
+
+			//Number - Width of the grid lines
+			scaleGridLineWidth: 1,
+
+			//Boolean - Whether to show horizontal lines (except X axis)
+			scaleShowHorizontalLines: true,
+
+			//Boolean - Whether to show vertical lines (except Y axis)
+			scaleShowVerticalLines: true,
+
+			//Boolean - Whether the line is curved between points
+			bezierCurve: true,
+
+			//Number - Tension of the bezier curve between points
+			bezierCurveTension: 0.4,
+
+			//Boolean - Whether to show a dot for each point
+			pointDot: true,
+
+			//Number - Radius of each point dot in pixels
+			pointDotRadius: 4,
+
+			//Number - Pixel width of point dot stroke
+			pointDotStrokeWidth: 1,
+
+			//Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+			pointHitDetectionRadius: 20,
+
+			//Boolean - Whether to show a stroke for datasets
+			datasetStroke: true,
+
+			//Number - Pixel width of dataset stroke
+			datasetStrokeWidth: 2,
+
+			//Boolean - Whether to fill the dataset with a colour
+			datasetFill: true,
+
+			display:true,
+
+			//String - A legend template
+			legendTemplate: "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+
+		};
+
+  		var data = {
+    		labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Setiembre", "Octubre", "Noviembre", "Diciembre"],
+    		datasets: 
+    		[
+    			{
+        			fillColor : "rgba(211, 84, 0,1.0)",
+        			pointHighlightStroke: "rgba(39, 174, 96,1.0)",
+        			data : obtenerUtilidades()
+    			},
+    			{
+       		 		fillColor : "rgba(41, 128, 185,1.0)",
+        			pointHighlightStroke: "rgba(39, 174, 96,1.0)",
+        			data: obtenerCostos()
+    			} 
+    		]
+		};
+
+  		//Chart.defaults.global.responsive = true;
+
+  		var ctx = $("#reporteUC").get(0).getContext("2d");
+
+  		var reporteChart = new Chart(ctx).Bar(data, options);
+	}
+
+	function reporteProductosMasVendidos () {
+
+		var data = [
+
+        {
+            value: 20,
+            color:"#637b85"
+        },
+        {
+            value : 30,
+            color : "#2c9c69"
+        },
+        {
+            value : 40,
+            color : "#dbba34"
+        },
+        {
+            value : 10,
+            color : "#c62f29"
+        }
+
+    ];
+
+		var ctx = $("#reporteProductosMasVendidos").get(0).getContext("2d");
+
+		var reportePMV = new Chart(ctx).Pie(data);
+
+	}
+
 	setTimeout(function () {
 
 		Tracker.autorun(reporteVC);
 		Tracker.autorun(reporteVM);
-	
+		Tracker.autorun(reporteUC);
+		Tracker.autorun(reporteProductosMasVendidos);
+
 	}, 1000)
 
 	
